2ab90afa91c52318f6eaf2446f5c648d6bce1c19
Mia Isaacs <mbi2110@cumc.columbia.edu>
2025-02-23 16:33:07 -0500

    finishing up lab report 5

--- homework5.R
+++ homework5.R
@@ -42,7 +42,51 @@ diag(q4matrix) = "x"
 q4matrix
 
 
+## question 5
 
+library(deSolve)
+
+SIR = function(t, state, parameters) {
+  with(as.list(c(state, parameters)), {
+    
+    dS = -beta * S * I / N
+    dI = beta * S * I / N - gamma * I
+    
+    list(c(dS, dI))
+  })
+}
+
+N = 1e5       
+I0 = 10       
+S0 = N - I0   
+gamma = 0.8  
+beta_vec = seq(0.5, 2, by = 0.1)  
+times = seq(1, 365, by = 1)    
+
+# define final epidemic size and R0
+final_sizes <- numeric(length(beta_vec))  
+R0_values <- beta_vec / gamma           
+
+# write for loop
+for (i in seq_along(beta_vec)) {
+  beta <- beta_vec[i]
+  
+  state <- c(S = S0, I = I0)
+  parameters <- c(beta = beta, gamma = gamma)
+  
+  out <- ode(y = state, times = times, func = SIR, parms = parameters)
+  
+  # final number of susceptibles
+  final_S <- out[nrow(out), "S"]
+  
+  # final epidemic size (total infected = N - final_S)
+  final_sizes[i] <- N - final_S
+}
+
+plot(R0_values, final_sizes, type = "b", pch = 16, col = "blue",
+     xlab = expression(R[0]), ylab = "Final Epidemic Size",
+     main = "Impact of R0 on Epidemic Size")
+grid()
 
 
 
