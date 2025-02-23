32244db28c84b3d4e92afc69edd1912f78887571
Mia Isaacs <mbi2110@cumc.columbia.edu>
2025-02-23 16:04:24 -0500

    working through lab report 5

--- homework5.R
+++ homework5.R
@@ -1,16 +1,45 @@
 
 # homework 5
 
+## question 1
+
+n = 9
+
+for (i in 1:n) {
+  cat(1:i, "\n")
+}
 
 
+## question 2
+
+n = 9
+
+for (i in 1:n) {
+  cat(strrep(i, i), "\n")
+}
 
 
+## question 3
+
+### input data
+out.daily = read.csv('~/Desktop/Modeling for ID/idm_week5/out.daily.csv')
+
+out.daily
+
+### aggregate into weeks
+out.daily$Week <- ceiling(out.daily$Day / 7)
+weekly_cases <- tapply(out.daily$Cases, out.daily$Week, sum)
+weekly_data <- data.frame(Week = names(weekly_cases), Weekly_Cases = weekly_cases)
+print(weekly_data)
 
 
+## question 4
 
-
-
-
+n = 5
+q4matrix <- matrix("",nrow = n, ncol = n)
+q4matrix[cbind(1:n, rev(1:n))] = "o"
+diag(q4matrix) = "x"
+q4matrix
 
 
 
